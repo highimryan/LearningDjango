4171eab342582c7fc6ffa9b0573069e125a54ade
Richard White <WHITER23@my.erau.edu>
2019-04-15 11:12:04 -0400

    added about page

--- HelloDjangoApp/views.py
+++ HelloDjangoApp/views.py
@@ -1,20 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-#imports a datetime
+# imports a datetime
 from datetime import datetime
+
+# Create your views here
+
 
 def index(request):
     now = datetime.now()
 
     return render(
         request,
-        "HelloDjangoApp/index.html",  # Relative path from the 'templates' folder to the template file
+        # Relative path from the 'templates' folder to the template file
+        "HelloDjangoApp/index.html",
         # "index.html", # Use this code for VS 2017 15.7 and earlier
         {
-            'title' : "Hello Django",
-            'message' : "Hello Django!",
-            'content' : " on " + now.strftime("%A, %d %B, %Y at %X")
+            'title': "Hello Django",
+            'message': "Hello Django!",
+            'content': " on " + now.strftime("%A, %d %B, %Y at %X")
         }
     )
 
-# Create your views here.
+
+def about(request):
+    return render(
+        request,
+        "HelloDjangoApp/about.html",
+        {
+            'title': "About HelloDjangoApp",
+            'content': "Example app page for Django."
+        }
+    )
--- settings.py
+++ settings.py
@@ -118,3 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['static']))
